9b217d6f44e7094775d30c9f5e93fb8a3e375cf0
Eduardo30TI <121513063+Eduardo30TI@users.noreply.github.com>
2023-03-29 09:12:21 -0300

    Ajuste função ROSSI

--- Automação/Comercial/Cadastro/BOT Cadastro/main.py
+++ Automação/Comercial/Cadastro/BOT Cadastro/main.py
@@ -265,7 +265,7 @@ def JOANIN(produtos:list):
 
             pass
 
-        temp_dict={'C16':'Produto','C17':'EAN13', 'F17':'Fabricante','C18':'Peso Líquido KG', 'F18':'Peso Bruto KG', 'C19':'Comprimento STK', 'C20':'Largura STK', 'C21': 'Altura STK', 'C23': 'SKU','C26':'Unid. VDA', 'C27':'Qtde Caixa', 'C28': 'DUN14', 'C29': 'Peso Caixa Bruto','C30': 'Peso Caixa Líquido','C31': 'Comprimento CMP','C32': 'Largura CMP', 'C33':'Altura CMP', 'C47':'NCM', 'C48':'CST VDA'}
+        temp_dict={'C16':'Produto','C17':'EAN13', 'F17':'Fabricante','C18':'Peso Líquido KG', 'F18':'Peso Bruto KG', 'C19':'Comprimento STK', 'C20':'Largura STK', 'C21': 'Altura STK', 'C23': 'SKU','C26':'Unid. VDA', 'C27':'Qtde Caixa', 'C28': 'DUN14', 'C29': 'Peso Caixa Bruto','C30': 'Peso Caixa Líquido','C31': 'Comprimento CMP','C32': 'Largura CMP', 'C33':'Altura CMP', 'C47':'NCM', 'C48':'CEST'}
         
         #preencher dados dos produtos
         for key,value in temp_dict.items():
@@ -415,6 +415,244 @@ def CHOCOLANDIA(produtos:list):
 
     pass
 
+def BERGAMINI(produtos: list):
+
+    name_arq='Cadastro BERGAMINI.xlsx'
+
+    df=sql.CriarTabela(kwargs=querys)
+
+    col_leach='bergamini'
+
+    path_base=os.path.join(os.getcwd(),'Planilhas','*.xlsx')
+
+    df['Produtos']=df['Produtos'].loc[df['Produtos']['SKU'].isin(produtos)]
+    
+    for arq in glob(path_base):
+
+        arq_name=os.path.basename(arq)
+
+        if str(arq_name).lower().find(col_leach)<0:
+
+            continue
+        
+        temp_path=arq
+
+        pass
+
+    sheet=load_workbook(temp_path)
+    sheet.active
+
+    sheet_names=sheet.sheetnames[-1]
+
+    range=sheet[sheet_names]
+
+    for c in df['Produtos']['Fotos'].tolist():
+
+        nome_arq=os.path.basename(c)
+
+        origem=c
+
+        destino=os.path.join(os.getcwd(),'Fotos',nome_arq)
+
+        if not os.path.exists(os.path.dirname(destino)):
+        
+            os.makedirs(os.path.dirname(destino))
+
+            pass
+
+        shutil.copy(origem,destino)
+
+        pass
+
+    if os.path.exists(os.path.dirname(destino)):
+
+        dir_name=os.path.basename(os.path.dirname(destino))
+
+        temp_path=os.path.dirname(destino)
+
+        shutil.make_archive(dir_name,'zip',temp_path)
+
+        shutil.rmtree(os.path.dirname(destino))
+
+        pass
+
+    #preencher planilha
+    for i,c in enumerate(df['Produtos']['SKU'].tolist()):
+        
+        if i==0:
+
+            name_sheet=str(c)
+            range.title=name_sheet
+
+            pass
+
+        else:
+
+            name_sheet=str(c)
+            range=sheet.copy_worksheet(range)
+            range.title=name_sheet
+            
+            pass
+
+        temp_dict={'AA12':'Razão Social'}
+        
+        #preencher dados do fornecedor
+        sheet_names=sheet.sheetnames[-1]
+
+        for key,value in temp_dict.items():
+            
+            range[key]=str(df['Fornecedores'][value].max())
+
+            #break
+
+            pass
+
+        temp_dict={'K6':'Produto','I8':'SKU','J10':'Fabricante','E12':'Qtde Caixa',4:'EAN13',10:'DUN14',8:'NCM'}
+
+        col_leach={'EAN13':9,'DUN14':26,'NCM':31}
+        
+        #preencher dados dos produtos
+        for key,value in temp_dict.items():
+
+            if key in [4,10,8]:
+
+                val=str(df['Produtos'].loc[df['Produtos']['SKU']==c,value].max())
+
+                if key==8:
+
+                    val=val.strip().replace('.','')
+
+                    pass
+
+                lista=[l for l in val]
+
+                indice=col_leach[value]
+
+                for i,j in enumerate(lista):
+                                        
+                    range.cell(row=key,column=indice+i,value=str(j))
+
+                    pass
+
+                pass
+
+            else:
+                                    
+                range[key]=str(df['Produtos'].loc[df['Produtos']['SKU']==c,value].max())
+
+                pass
+            #break
+
+            pass
+        
+        pass
+
+
+    sheet.save(name_arq)
+
+    temp_path=os.path.join(os.getcwd(),name_arq)
+    
+    return temp_path
+
+    pass
+
+def ROSSI(produtos: list):
+
+    name_arq='Cadastro ROSSI.xlsx'
+
+    df=sql.CriarTabela(kwargs=querys)
+
+    col_leach='rossi'
+
+    path_base=os.path.join(os.getcwd(),'Planilhas','*.xlsx')
+
+    df['Produtos']=df['Produtos'].loc[df['Produtos']['SKU'].isin(produtos)].reset_index()
+    
+    for arq in glob(path_base):
+
+        arq_name=os.path.basename(arq)
+
+        if str(arq_name).lower().find(col_leach)<0:
+
+            continue
+        
+        temp_path=arq
+
+        pass
+
+    sheet=load_workbook(temp_path)
+    sheet.active
+
+    sheet_names=sheet.sheetnames[-1]
+
+    range=sheet[sheet_names]
+
+    for c in df['Produtos']['Fotos'].tolist():
+
+        nome_arq=os.path.basename(c)
+
+        origem=c
+
+        destino=os.path.join(os.getcwd(),'Fotos',nome_arq)
+
+        if not os.path.exists(os.path.dirname(destino)):
+        
+            os.makedirs(os.path.dirname(destino))
+
+            pass
+
+        shutil.copy(origem,destino)
+
+        pass
+
+    if os.path.exists(os.path.dirname(destino)):
+
+        dir_name=os.path.basename(os.path.dirname(destino))
+
+        temp_path=os.path.dirname(destino)
+
+        shutil.make_archive(dir_name,'zip',temp_path)
+
+        shutil.rmtree(os.path.dirname(destino))
+
+        pass
+
+    col={'Altura CMP':22,'Largura CMP':23,'Comprimento CMP':24}
+
+    #preencher planilha
+    lista = df['Produtos'].index.tolist()
+    for i,c in enumerate(['SKU','Produto','Fabricante','Unid. VDA','Qtde Caixa','EAN13','DUN14','NCM','CST VDA','CEST','Altura CMP','Largura CMP','Comprimento CMP']):
+        indice=6
+        for l in lista:
+
+            val = df['Produtos'].loc[l,c] if not c in ['NCM','CEST'] else str(df['Produtos'].loc[l,c]).replace('.','')
+            
+            if c in col.keys():
+
+                range.cell(row=indice,column=col[c],value=val*100)
+                indice+=1
+
+                pass
+
+            else:
+                
+                range.cell(row=indice,column=i+1,value=val)
+                indice+=1
+
+                pass
+
+
+            pass
+
+        pass
+    
+    sheet.save(name_arq)
+
+    temp_path=os.path.join(os.getcwd(),name_arq)
+
+    return temp_path
+
+    pass
 
 if __name__=='__main__':
         
